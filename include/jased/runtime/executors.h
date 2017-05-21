207c58c4614732f328b310bc0b773b62c3c9a460
Vladimir <vprovalov@ya.ru>
2017-05-21 05:43:29 +0300

    refactored executors.h

--- include/jased/runtime/executors.h
+++ include/jased/runtime/executors.h
@@ -9,9 +9,11 @@
 
 #define RT_ERR int 
 
-struct regex_match_args {
-	regex_t regex;
-}; 
+/*                 
+     Executors - abstract objects which contains jased commands with their arguments.
+     Runners - functions which iterpret executors.      
+     Cleaners - functions which clean executors mem. 
+*/ 
 
 struct regex_sub_args {
 	regex_t regex;
@@ -24,7 +26,6 @@ struct string_param_args {
 };
 
 typedef union cmd_args {
-	struct regex_match_args*     reg_match;
 	struct regex_sub_args*   	 reg_sub;
 	struct string_param_args*    string_param;
 	condition_args_t*     		 condition; 
@@ -38,16 +39,12 @@ typedef struct runtime_ctx {
 
 typedef RT_ERR (*no_params_cmd_t) ( jased_ctx_t* const );
 typedef RT_ERR (*string_param_cmd_t) ( jased_ctx_t* const, string_buffer_t* const );
-typedef RT_ERR (*regex_match_cmd_t) ( jased_ctx_t* const, regex_t const );
 typedef RT_ERR (*regex_sub_cmd_t) ( jased_ctx_t* const, regex_t const, string_buffer_t* const, int const );
-typedef RT_ERR (*condition_cmd_t) ( jased_ctx_t* const, condition_args_t* cond_args  );
 
 typedef union cmd_type {
 	no_params_cmd_t    run_no_args;	
 	string_param_cmd_t run_str_arg;
-	regex_match_cmd_t  run_regmatch;
 	regex_sub_cmd_t    run_regsub;
-	condition_cmd_t    run_condition;
 } cmd_type_t;
 
 typedef struct executor {
@@ -58,7 +55,7 @@ typedef struct executor {
 } executor_t;
 
 
-#define DECLARE_EXECUTOR( name ) \
+#define DECLARE_RUNNER( name ) \
 RT_ERR name( executor_t* const executor );
 
 #define DECLARE_CLEANER( name ) \
@@ -66,16 +63,37 @@ void name( executor_t* executor );
 
 executor_t* executor_new();
 
-DECLARE_EXECUTOR( exec_no_params );
+DECLARE_RUNNER( exec_no_params );
 DECLARE_CLEANER( clean_no_params );
 
-DECLARE_EXECUTOR( exec_one_param_str );
+DECLARE_RUNNER( exec_one_param_str );
 DECLARE_CLEANER( clean_one_param_str );
 
-DECLARE_EXECUTOR( exec_regex_sub );
+DECLARE_RUNNER( exec_regex_sub );
 DECLARE_CLEANER( clean_regex_sub );
 
-DECLARE_EXECUTOR( exec_condition );
-DECLARE_CLEANER( clean_condition );
+
+DECLARE_RUNNER( exec_regex_sub );
+DECLARE_CLEANER( clean_regex_sub );
+
+/* CONDITIONS */
+
+DECLARE_RUNNER( exec_line_condition );
+DECLARE_CLEANER( clean_line_condition );
+
+DECLARE_RUNNER( exec_line_range_condition );
+DECLARE_CLEANER( clean_line_range_condition );
+
+DECLARE_RUNNER( exec_regmatch_condition );
+DECLARE_CLEANER( clean_regmatch_condition );
+
+DECLARE_RUNNER( exec_regmatch_range_condtion );
+DECLARE_CLEANER( clean_regmatch_range_condtion );
+
+DECLARE_RUNNER( exec_line_regmatch_condition );
+DECLARE_CLEANER( clean_line_regmatch_condition );
+
+DECLARE_RUNNER( exec_regmatch_line_condition );
+DECLARE_CLEANER( clean_regmatch_line_condition );
 
 #endif
