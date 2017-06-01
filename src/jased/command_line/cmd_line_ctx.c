1267bb6d0de21a0dc087a5bb7ddcb277076980fd
Vladimir <vprovalov@ya.ru>
2017-06-01 19:17:11 +0300

    refactored. By default 1 arg - is a script, if no scripts or filescript was not added by using keys -f or -e

--- src/jased/command_line/cmd_line_ctx.c
+++ src/jased/command_line/cmd_line_ctx.c
@@ -3,7 +3,9 @@
 #include <malloc.h>
 
 #include "jased/command_line/cmd_line_ctx.h"
+#include "jased/constants.h"
 #include "jased/io/io.h"
+
 
 #define DEFAULT_COUNT 10 
 
@@ -23,9 +25,10 @@ void cmdline_ctx_delete( struct cmdline_ctx* ctx) {
     free( ctx );
 }
 
+#define CMDLINE_OPTIONS "ne:f:"
+
 struct cmdline_ctx*
 get_cmdline_ctx( int const argc, char** argv ) {
-    char* const cmdline_options = "ne:f:";
     struct cmdline_ctx* ctx = malloc( sizeof(struct cmdline_ctx) );
     int opt;
 
@@ -33,25 +36,26 @@ get_cmdline_ctx( int const argc, char** argv ) {
     ctx-> args_scripts =     malloc( sizeof(char*) * DEFAULT_COUNT );
     ctx-> source_filenames = NULL;
 
-    ctx-> source_files_count = 0;
-    ctx-> script_files_count = 0;
-    ctx-> args_scripts_count = 0;
-    ctx-> is_default_output_enable = 1;
+    ctx-> source_files_count =
+        ctx-> script_files_count = 
+            ctx-> args_scripts_count = 0;
+
+    ctx-> is_default_output_enable = DEFAULT_OUTPUT_ENABLE;
 
     if ( argc == 1 ) {
-       println("jased syntax: jased [-n] [-e script ]... [ -f scriptfile ]... [ sourcefile ]...");
+       println( CMDLINE_DEFAULT_MSG );
        cmdline_ctx_delete( ctx );
        return NULL;
     }
 
 
     opterr = 1; /* getopt handle errors */
-    while ( (opt = getopt(argc, argv, cmdline_options)) != -1 ) {
+    while ( (opt = getopt(argc, argv, CMDLINE_OPTIONS)) != -1 ) {
         switch(opt) {
             case SCRIPT_KEY: {
                 if ( ctx-> args_scripts_count == DEFAULT_COUNT ) {
                     cmdline_ctx_delete( ctx ); 
-                    printerr("jased: too many arguments scripts");
+                    printerr( TOO_MANY_ARGUMENTS_SCRIPTS_MSG );
                     return NULL;
                 }
 
@@ -63,7 +67,7 @@ get_cmdline_ctx( int const argc, char** argv ) {
             case SCRIPTFILE_KEY: {
                 if ( ctx-> script_files_count == DEFAULT_COUNT ) {
                     cmdline_ctx_delete( ctx ); 
-                    printerr("jased: too many arguments file scripts");
+                    printerr( TOO_MANY_ARGUMENTS_FILESCRIPTS_MSG );
                     return NULL;
                 }
 
@@ -73,10 +77,15 @@ get_cmdline_ctx( int const argc, char** argv ) {
             }
 
             case DEFAULT_OUTPUT_KEY: {
-                ctx-> is_default_output_enable = 0;
+                ctx-> is_default_output_enable = DEFAULT_OUTPUT_DISABLE;
                 break;
             }
         }
+    }
+
+    if ( ctx-> args_scripts_count == 0 && ctx-> script_files_count == 0 ) {
+        ctx-> args_scripts_count++;
+        ctx-> args_scripts[0] = argv[optind++];
     }
 
     ctx-> source_filenames = argv + optind; 
