3d69b401f10bac5f7d287d096b5dbab8b03de8d7
Vladimir <vprovalov@ya.ru>
2017-06-01 19:17:40 +0300

    header for all global constants

---
+++ include/jased/constants.h
@@ -0,0 +1,14 @@
+#ifndef JASED_CONSTANTS
+#define JASED_CONSTANTS
+
+/* cmdline args messages */
+#define CMDLINE_DEFAULT_MSG "jased syntax: jased [-n] [-e script ]... [ -f scriptfile ]... [ sourcefile ]..."
+#define TOO_MANY_ARGUMENTS_SCRIPTS_MSG "jased: too many arguments scripts"
+#define TOO_MANY_ARGUMENTS_FILESCRIPTS_MSG "jased: too many arguments file scripts"
+
+/* default output */
+#define DEFAULT_OUTPUT_ENABLE 1
+#define DEFAULT_OUTPUT_DISABLE 0
+
+#endif
+
