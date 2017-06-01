11c0459c808ad87dc74cc2d48231717d60152483
Vladimir <vprovalov@ya.ru>
2017-06-01 20:19:59 +0300

    refactored skip_spaces

--- src/jased/parser/util.c
+++ src/jased/parser/util.c
@@ -1,34 +1,15 @@
+#include <ctype.h>
 #include "jased/parser/util.h"
-
-#define IS_SPACE( sym ) \
-	sym == ' ' || sym == '\t' || sym == '\n'
 
 int skip_spaces( chars_queue_t* const cqueue ) {	
 	int i = 0;
 	while ( !cqueue_is_empty(cqueue) ) {
 		char const sym = cqueue_gettop(cqueue);
 
-		if ( IS_SPACE(sym) ) {
+		if ( isspace(sym) ) {
 			i++; cqueue_getc(cqueue);
 		} else return i;
 	}
 
 	return -1;
 }
-
-/*static size_t parse_operand( char const* line, size_t const length, char const end_sym ) {
-	size_t i = 0;
-	size_t chieldchars_count = 0;
-		
-	for ( ; i < length; i++ ) {
-		if ( line[i] == end_sym && (chieldchars_count == 0 || chieldchars % 2 != 0 ) )
-				break;
-
-		chieldchars_count = line[i] == CMD_SHIELD_CHAR ? 
-			chieldchars + 1 : 0;
-		
-	}
-
-	return i;
-}*/
-
